81e6d9e5780187eb24f0c65f42910b1a1f48241d
cfaw1 <44452099+cfaw1@users.noreply.github.com>
2018-10-31 10:54:34 +0000

    Update myCollatz.m

--- myCollatz.m
+++ myCollatz.m
@@ -1,18 +1,18 @@
-function [period, a_max] = myCollatz(a0)
-% Initialising variables    
-    a = [a0];
+function [period, a_max] = myCollatz(a0)   
     x = a0;
-% Creating sequence as array    
+    period = 0;
+    a_max = a0;
+    
     while (x > 1)
         if mod(x,2) == 0
-            x = x / 2;
-            a = [a x];
+            x = x / 2; 
         elseif mod(x,2) == 1
             x = 3 * x + 1;
-            a = [a x];
-        end   
-    end
-% Getting output    
-    period = length(a) - 1;
-    a_max = max(a);
+            if x > a_max
+                a_max = x;
+            end           
+        end
+        period = period + 1;
+    end  
+    
 end
